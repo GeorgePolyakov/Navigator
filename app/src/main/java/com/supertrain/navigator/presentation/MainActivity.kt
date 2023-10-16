9c6bb5b2d23eaff0cbe587cd1f87f35ae69f67f7
Egor <egor1995polyakov@gmail.com>
2023-10-16 16:09:05 +0300

    init Activity ViewModel

--- app/src/main/java/com/supertrain/navigator/presentation/MainActivity.kt
+++ app/src/main/java/com/supertrain/navigator/presentation/MainActivity.kt
@@ -1,30 +1,26 @@
 package com.supertrain.navigator.presentation
 
-import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import android.view.View
-import androidx.activity.viewModels
-import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentManager
-import androidx.lifecycle.ViewModelProvider
 import com.supertrain.navigator.R
+import com.supertrain.navigator.UiActions
 import com.supertrain.navigator.databinding.ActivityMainBinding
 import com.supertrain.navigator.nav.ActivityScopeViewModel
-import com.supertrain.navigator.nav.MainNavigator
+import com.supertrain.navigator.nav.MediateNavigator
 import com.supertrain.navigator.nav.StackFragmentNavigator
-import com.supertrain.navigator.presentation.base.BaseFragment
-import com.supertrain.navigator.presentation.base.ViewModelFactory
+import com.supertrain.navigator.presentation.base.viewModelCreator
 import com.supertrain.navigator.presentation.hello.HelloFragment
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var navigator : StackFragmentNavigator
 
-    private val activityViewModel by viewModels<ActivityScopeViewModel> {
-        ViewModelProvider.AndroidViewModelFactory(application)
-        }
+    private val activityViewModel by viewModelCreator <ActivityScopeViewModel> {
+     ActivityScopeViewModel(
+         uiActions = UiActions.AndroidUiActions(applicationContext),
+         navigator  = MediateNavigator()
+     )
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -51,7 +47,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onPause() {
         super.onPause()
-        navigator.whenActivityActive.mainActivity = null
+        activityViewModel.navigator.setTargetNavigator(null) // clear target navigator when other viewModels will call commands theese comands will be in navigator list
     }
 
     override fun onDestroy() { // we may not call callback methodes we can use lifecycle owner
